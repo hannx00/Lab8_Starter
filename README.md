bf2fac55b70134939a28649580e8795f80b86333
hannx00 <han001@ucsd.edu>
2021-11-14 21:51:06 -0800

    file upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 8 - Starter
+
+
+1) Manually run them locally before pushing code
+Because the project has many components, it is the most optimal to test each feature thoroughly in a local server before implementing a new one. Ideally,
+the tests will be written after completing the function, so we will know right away if it works as expected before pushing code.
+2) Yes
+3) No because of the limitation of unit testing(Cannot test how these individual components interact with each other on an application/feature level). Testing the send feature requires us to interact with the components before and after sending the texts.
+4) Yes because the max length is simply a requirement of the input. Since it doesn't involve interacting with other components, unit testing would be fitting here. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,90 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// phoneNumber
+test('is phonenumber 911', () => {
+    expect(functions.isPhoneNumber(911)).toBe(false);
+});
+
+test('is phonenumber fruity', () => {
+    expect(functions.isPhoneNumber("fruity")).toBe(false);
+});
+
+test('is phonenumber 818-818-8188', () => {
+    expect(functions.isPhoneNumber("818-818-8188")).toBe(true);
+});
+
+test('is phonenumber 000-000-0000', () => {
+    expect(functions.isPhoneNumber("000-000-0000")).toBe(true);
+});
+
+
+// isEmail
+test('isEmail 911', () => {
+    expect(functions.isEmail("911")).toBe(false);
+});
+
+test('isEmail hahe@@', () => {
+    expect(functions.isEmail("hahe@@")).toBe(false);
+});
+
+test('isEmail abc@gmail.com', () => {
+    expect(functions.isEmail("abc@gmail.com")).toBe(true);
+});
+
+test('isEmail abc@yahoo.com', () => {
+    expect(functions.isEmail("abc@yahoo.com")).toBe(true);
+});
+
+// isStrongPassword
+
+test('isStrongPassword ^_^~', () => {
+    expect(functions.isStrongPassword("^_^~")).toBe(false);
+});
+
+test('isStrongPassword apestogetherstrong', () => {
+    expect(functions.isStrongPassword("apestogetherstrong")).toBe(false);
+});
+
+test('isStrongPassword good_pw', () => {
+    expect(functions.isStrongPassword("good_pw")).toBe(true);
+});
+
+test('isStrongPassword verygoodpw00_', () => {
+    expect(functions.isStrongPassword("verygoodpw00_")).toBe(true);
+});
+
+// isDate
+test('isDate ^_^~', () => {
+    expect(functions.isDate("^_^~")).toBe(false);
+});
+
+test('isDate 12/20/2222', () => {
+    expect(functions.isDate("12/20/2222")).toBe(true);
+});
+
+test('isDate 00 / 00 / 0000', () => {
+    expect(functions.isDate("00 / 00 / 0000")).toBe(false);
+});
+
+test('isDate 00/00/0000', () => {
+    expect(functions.isDate("00/00/0000")).toBe(true);
+});
+
+// isHexColor
+test('isHexColor #FFCC00', () => {
+    expect(functions.isHexColor("#FFCC00")).toBe(true);
+});
+
+test('isHexColor #FC0', () => {
+    expect(functions.isHexColor("#FC0")).toBe(true);
+});
+
+
+test('isHexColor #FCAQEC', () => {
+    expect(functions.isHexColor("#FCAQEC")).toBe(false);
+});
+
+test('isHexColor #xyz', () => {
+    expect(functions.isHexColor("#xyz")).toBe(false);
+});
